7687f68ea320839912e566d2cb462f85644c96d7
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-03-31 19:51:17 -0400

    docs: Setup integers and strings to recall data in csvfile

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,14 @@ import os
 import csv
 import statistics 
 
+#Setup variables integers and strings to start at 0
+Month_Count= 0
+Profil_Losses_Tot = 0
+Profit_Losses_Diff_Average = 0.0
+Greatest_Incr = 0
+Best_month = ''
+Greatest_Decr = 0
+Worst_Month = ''
+
+month_month_change = []
+change_diff = []
