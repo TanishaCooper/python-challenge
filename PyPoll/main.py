ef278470325d34c078e4e7d72e1ee4880624c9c0
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-04-01 13:14:44 -0400

    docs: Read each row after header to get total votes. #print tested total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,10 +3,11 @@ import os
 import csv
 
 # Set variables
-
 Total_votes = 0
+Candidate_votes = {}
 
-# Set path for csvfile
+
+# Set path for election_data csvfile
 election_data_csv = os.path.join("PyPoll/PyPoll/Resources/election_data.csv")
 
 #Open and read election_data.csv
@@ -15,4 +16,15 @@ with open(election_data_csv) as read_csvfile:
 
     # Read header row first
     csv_header = next(read_csvfile)
-    print(f'Header: {csv_header}')
+    #print(f'Header: {csv_header}')
+
+    # Read each row after header (1) to total votes
+    for row in csv_reader:
+        Total_votes += 1
+        if row[2] not in Candidate_votes:
+            Candidate_votes[row[2]] = 1
+        else: 
+            Candidate_votes[row[2]] =+ 1
+
+#Test print output
+#print(str(Total_votes))
