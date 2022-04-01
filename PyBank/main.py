01f0d1ed5373da4570af4885cd912443eddb0b29
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-03-31 20:04:38 -0400

    docs: Read through each data after header to analyze

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,10 +28,28 @@ with open(budget_data_csv) as csvfile:
 
 # Track month changes
 for x in range(len(change_diff)-1):
-    Monthlychng = (change[1+1] - change[i])
+    Monthlychng = (change[x+1] - change[x])
     month_month_change.append(Monthlychng)
 
 avgchange = statistics.mean(month_month_change)
+
+
+# Read through each row of data after the header
+for row in csv_reader:
+    #print (row)
+    Month_Count += 1
+    Profil_Losses_Tot += int(row[1])
+
+    if int(row[1]) > Greatest_Incr:
+        Best_month = (row[0])Greatest_Incr = int(row[1])
+
+    elif int(row[1]) < Greatest_Decr:
+        Worst_Month = (row[0])
+        Greatest_Decr = int(row[1])
+
+    change_diff.append(int(row[1]))
+
+
 
 
 
