9793ee25b4578780be92c834f149c78a5b741642
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-04-01 12:42:45 -0400

    test: Header prints to test reader

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,18 @@
 #Modules
 import os
 import csv
+
+# Set variables
+
+Total_votes = 0
+
+# Set path for csvfile
+election_data_csv = os.path.join("PyPoll/PyPoll/Resources/election_data.csv")
+
+#Open and read election_data.csv
+with open(election_data_csv) as read_csvfile:
+    csv_reader = csv.reader(read_csvfile, delimiter=",")
+
+    # Read header row first
+    csv_header = next(read_csvfile)
+    print(f'Header: {csv_header}')
