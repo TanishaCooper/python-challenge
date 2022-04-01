a1d3719fcef947b283a0b90c6f58e536e61921f4
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-03-31 20:21:05 -0400

    fix: define Monthlychng variable

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,28 +26,36 @@ with open(budget_data_csv) as csvfile:
     csv_header = next(csvfile)
     #print(f'Header: {csv_header}')
 
-# Track month changes
-for x in range(len(change_diff)-1):
-    Monthlychng = (change[x+1] - change[x])
-    month_month_change.append(Monthlychng)
-
-avgchange = statistics.mean(month_month_change)
-
 
 # Read through each row of data after the header
-for row in csv_reader:
-    #print (row)
-    Month_Count += 1
-    Profil_Losses_Tot += int(row[1])
+    for row in csv_reader:
+        #print (row)
+        Month_Count += 1
+        Profil_Losses_Tot += int(row[1])
 
-    if int(row[1]) > Greatest_Incr:
-        Best_month = (row[0])Greatest_Incr = int(row[1])
+        if int(row[1]) > Greatest_Incr:
+          Best_month = (row[0])
+          Greatest_Incr = int(row[1])
 
-    elif int(row[1]) < Greatest_Decr:
-        Worst_Month = (row[0])
-        Greatest_Decr = int(row[1])
+        elif int(row[1]) < Greatest_Decr:
+            Worst_Month = (row[0])
+            Greatest_Decr = int(row[1])
 
-    change_diff.append(int(row[1]))
+        change_diff.append(int(row[1]))
+
+    # Track month changes
+    for i in range(len(change_diff)-1):
+        Monthlychng = (change[i+1] - change[i])
+        month_month_change.append(Monthlychng)
+
+    avgchange = statistics.mean(month_month_change)
+
+# Setup output view
+print("Financial Analysis")
+print("----------------------------------")
+
+print("Total Months: " + str(Month_Count))
+print("Average Change is: $" + str(float(avgchange)))
 
 
 
